74df42b46c67a7ff7f490e87638df504d714b52b
屈沛昕 <91247091+ftbb100@users.noreply.github.com>
2023-11-06 17:25:36 +0800

    Create cattoon.py

---
+++ cattoon.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+# 1. 读取图像
+image = cv2.imread('108390.jpg')
+
+# 2. 边缘检测
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+
+# 3. 转换为彩色卡通风格
+color = cv2.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
+cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+# 4. 保存结果
+cv2.imwrite('cartoon_output.jpg', cartoon)
+
+cv2_imshow(cartoon)
